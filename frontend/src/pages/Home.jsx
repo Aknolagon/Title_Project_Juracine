292fae533db08767c8e1176c564c6e6ce74d610a
Pras <kalki.prasanna.co@gmail.com>
2024-02-01 11:14:05 +0100

    front

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import "../styles/Home.scss";
+import SearchBar from "../components/SearchBar";
+import NavBar from "../components/NavBar";
+import UpcomingMovies from "../components/Categories/UpcomingMovies";
+import TrendingMovies from "../components/Categories/TrendingMovies";
+import TrendingSeries from "../components/Categories/TrendingSeries";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <div className="home">
+      <div>
+        <NavBar />
+        <div>
+          <SearchBar />
+        </div>
+        <div>
+          <TrendingMovies />
+        </div>
+        <div>
+          <TrendingSeries />
+        </div>
+        <div>
+          <UpcomingMovies />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
---
+++ frontend/src/pages/Movies.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import SearchBar from "../components/SearchBar";
+import "../styles/Movies.scss";
+import CategoryMovies from "../components/Categories/CategoryMovies";
+import TrendingMovies from "../components/Categories/TrendingMovies";
+import UpcomingMovies from "../components/Categories/UpcomingMovies";
+import Footer from "../components/Footer";
+
+function Movies() {
+  return (
+    <div className="movies">
+      <NavBar />
+      <SearchBar />
+      <TrendingMovies />
+      <CategoryMovies />
+      <UpcomingMovies />
+      <Footer />
+    </div>
+  );
+}
+
+export default Movies;
